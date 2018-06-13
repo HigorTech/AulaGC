7538f3035b85d24fa954c8b27cafececd54ef50b
Higpr Paulo <higortech159@gmail.com>
2018-06-12 22:06:20 -0300

    Isso é apenas um teste.

---
+++ Biblioteca/src/biblioteca/Usuario.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author Higor
+ */
+public class Usuario {
+    
+    private String nome;
+    private String cpf;
+    
+    
+    Usuario(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+        
+        
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
